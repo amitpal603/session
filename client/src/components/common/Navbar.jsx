0528feba6b3b939e04853cb138a53885c9b686e6
amitpal603-ops <ap036776@gmail.com>
2025-10-23 01:45:12 +0530

    username first letter capital

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../context/AuthoContext';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
-
+ console.log(user)
   return (
     <nav className="bg-blue-600 text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4">
@@ -16,7 +16,7 @@ const Navbar = () => {
           <div className="flex items-center space-x-4">
             {user ? (
               <>
-                <span>Welcome, {user.username}</span>
+                <span>Welcome {user.username.charAt(0).toUpperCase()}</span>
                 <Link
                   to="/profile"
                   className="hover:bg-blue-700 px-3 py-2 rounded"
