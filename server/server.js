1cd42fbc02050bdea07216b2cd71e4de5ed3eb0a
amitpal603-ops <ap036776@gmail.com>
2025-10-23 11:47:45 +0530

    listen post correct

--- server/server.js
+++ server/server.js
@@ -35,5 +35,5 @@ app.get('/api/health', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Backend server running on port ${PORT}`);
+  console.log(`Backend server running on port http://localhost:${PORT}`);
 });
